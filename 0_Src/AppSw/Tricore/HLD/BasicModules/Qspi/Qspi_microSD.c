f9e81bafbc84191da6d70788a1dbc711af7ab10b
Hohyon_Choi <Hohyon_Choi@222.99.10.87>
2020-03-30 17:05:33 +0900

    New source & header file for microSD functions

---
+++ 0_Src/AppSw/Tricore/HLD/BasicModules/Qspi/Qspi_microSD.c
@@ -0,0 +1,10 @@
+/*
+ * Qspi_microSD.c
+ *
+ *  Created on: 2020. 3. 30.
+ *      Author: Hohyon_Choi
+ */
+
+
+
+
---
+++ 0_Src/AppSw/Tricore/HLD/BasicModules/Qspi/Qspi_microSD.h
@@ -0,0 +1,15 @@
+/*
+ * Qspi_microSD.h
+ *
+ *  Created on: 2020. 3. 30.
+ *      Author: Hohyon_Choi
+ */
+
+#ifndef _QSPI_MICROSD_H_
+#define _QSPI_MICROSD_H_
+
+
+
+
+
+#endif /* 0_SRC_APPSW_TRICORE_HLD_BASICMODULES_QSPI_QSPI_MICROSD_H_ */
